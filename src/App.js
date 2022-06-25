a9756a2c1fe3be51975d7fc45bf576cfe61f19ba
paulrogers <paul303808909@protonmail.com>
2022-06-25 13:51:33 +0100

    run prettier / optimise imports

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import {useEffect, useState} from "react";
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import { useEffect, useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Tasks from "./components/Tasks";
--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,12 @@
-import {Link} from "react-router-dom";
-import Tag from "./Tag"
+import { Link } from "react-router-dom";
+import Tag from "./Tag";
 
 const About = () => {
   return (
     <div>
       <h4>Version 1.0.0</h4>
       <Link to="/">Back</Link>
-        <Tag text={"TESTING 123"}/>
+      <Tag text={"TESTING 123"} />
     </div>
   );
 };
