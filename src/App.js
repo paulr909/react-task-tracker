0417d2fdd825f5c8fb3a3c6f90b1afbed6d6952e
paulrogers <paul303808909@protonmail.com>
2022-06-28 19:58:51 +0100

    rm commented code

--- src/App.js
+++ src/App.js
@@ -43,10 +43,6 @@ const App = () => {
     const data = await res.json();
 
     setTasks([...tasks, data]);
-
-    // const id = Math.floor(Math.random() * 10000) + 1
-    // const newTask = { id, ...task }
-    // setTasks([...tasks, newTask])
   };
 
   // Delete Task
